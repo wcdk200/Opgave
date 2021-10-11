31ae569073164e9451e2c3ebe7d5d64040066333
wcdk200 <sebguld@gmail.com>
2021-10-11 12:19:45 +0200

    m

---
+++ Opgave 5/opgave5,12.py
@@ -0,0 +1,11 @@
+fag1= "programmering"
+print(fag1)
+fag2= "indleredSystemmer"
+print(fag1,fag2)
+fag3= "virksomhed"
+print(fag1,fag2,fag3)
+li= [fag1,fag2]
+print(li)
+#koden fra opg. 5,11
+
+print(len(li))
