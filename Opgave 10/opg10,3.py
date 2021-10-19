6dc75d50e1501a61756b169e2eca2b05fffbfefd
wcdk200 <sebguld@gmail.com>
2021-10-19 13:37:54 +0200

    m

---
+++ Opgave 10/opg10,3.py
@@ -0,0 +1,13 @@
+t = [20,4,31.6,49.9,60.1,61.5,62,69,69.1,69]
+
+tt = len(t)
+
+ttt = sum(t)
+#Her er medianen
+tttt = ttt / tt
+
+print(tttt)
+#lige præsis har lavet det samme to gang men ellers er det her gennemsnit
+tat = ttt/tt
+
+print(tat)
