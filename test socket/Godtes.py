86cbcc0a6158f377506196f5deb745917ab78ddd
wcdk200 <sebguld@gmail.com>
2021-11-11 13:08:52 +0100

    m

--- test socket/Godtes.py
+++ test socket/Godtes.py
@@ -21,7 +21,7 @@ FPS = 60 #bestemmer maks fps. bare så det ikke ender med at lagge noget ud.
 skt = socket.socket() #Forbinder til Rasp... Husk at tjekke om ip'en og porten er den rigtige
 
 host = "192.168.1.249" #Ip-addressen for Raspberry Pi
-port = 4200
+port = 4200 #og ja det her er porten
 
 skt.connect((host, port))
 
@@ -29,7 +29,7 @@ def draw_windue(styr): #Her er noget styring til skærmen
     WIN.fill(skærmfarve) #Det her er baggrundsfarven
     WIN.blit(tar, (styr.x, styr.y)) #Bare et billede af et rat 
     pygame.display.update()
-a
+
 def main(): #Det vigtige kode er her
     styr = pygame.Rect(195, 0, 507, 676)
 
@@ -42,11 +42,20 @@ def main(): #Det vigtige kode er her
                 gameLoop = False
             elif event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
                 keys = pygame.key.get_pressed() #Den her siger hvad der skal gøres, finds forskellige taster bliver trykket på
-                if keys[pygame.K_w]: #Fuldskrue frem ad
-                    data = "100,100" #V sendes først også H
+
+                if keys[pygame.K_w] and keys[pygame.K_d]: 
+                    data = "100,55"
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
-
+                elif keys[pygame.K_w] and keys[pygame.K_a]:
+                    data = "80,100"
+                    nyt_data = data.encode("UTF-8")
+                    skt.sendall(nyt_data)
+                    
+                elif keys[pygame.K_w]: #Fuldskrue frem ad
+                    data = "100,75" #V sendes først også H muligvis den anden vej rundt
+                    nyt_data = data.encode("UTF-8")
+                    skt.sendall(nyt_data)
                 elif keys[pygame.K_q]: #Hvis vi skal have blink lys eller bare bruge q og e til noget
                     data = "lysV"
                     nyt_data = data.encode("UTF-8")
@@ -60,10 +69,6 @@ def main(): #Det vigtige kode er her
                     data = "0,100"
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
-                elif keys[pygame.K_w] and [pygame.K_a]:
-                    data = "80,100"
-                    nyt_data = data.encode("UTF-8")
-                    skt.sendall(nyt_data)
                     
                 elif keys[pygame.K_s]: #Fuldstop ind til videre -tror jeg
                     data = "0,0"
@@ -72,10 +77,6 @@ def main(): #Det vigtige kode er her
 
                 elif keys[pygame.K_d]: #Her dreje til højre man gør
                     data = "100,0"
-                    nyt_data = data.encode("UTF-8")
-                    skt.sendall(nyt_data)
-                elif keys[pygame.K_w] and keys[pygame.K_d]:
-                    data = "100,80"
                     nyt_data = data.encode("UTF-8")
                     skt.sendall(nyt_data)
 
