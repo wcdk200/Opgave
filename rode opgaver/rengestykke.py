9ef64370e20cdefbcad421c030314be5ea2d2ddb
wcdk200 <sebguld@gmail.com>
2022-01-13 18:51:01 +0100

    b

--- rode opgaver/rengestykke.py
+++ rode opgaver/rengestykke.py
@@ -2,5 +2,5 @@ tal = input("Skriv et tal her som skal ganges med 4: ")
 
 regnestykke = 4* float(tal)
 sRejgestykke = str(regnestykke)
-
-print("tallet gange med 4 er: ", sRejgestykke)
+ 
+print("tallet gange med 4 er : ", sRejgestykke)
