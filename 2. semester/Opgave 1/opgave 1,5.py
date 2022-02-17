9c135cce76dd0ab9a15979fdf9e8e764ee2069ba
wcdk200 <sebguld@gmail.com>
2022-02-17 22:28:57 +0100

    m

--- 2. semester/Opgave 1/opgave 1,5.py
+++ 2. semester/Opgave 1/opgave 1,5.py
@@ -1,1 +1,10 @@
-l = [10,8,7,9,8,9]
+l = [10,8,7,9,8,9,10,6,7,8,11,3,4,17]
+print("Vi har en liste her, som vi vil finde de 10 første tal af: ", l)
+
+def cut10():
+    x = slice(10)
+    lt = (l[x])
+    tt = tuple(lt)
+    print("De første 10 tal er: ",tt)
+
+cut10()
