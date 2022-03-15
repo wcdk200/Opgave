da57739a426ea6c7ed2fe76f6b20af5b0aa5fc78
wcdk200 <sebguld@gmail.com>
2022-03-15 12:22:27 +0100

    m

---
+++ 2. semester/opgave 5/opgave5,1.py
@@ -0,0 +1,9 @@
+#Har ikke lave opgave 4,1-4,3 så hopper bare ud i det her fra
+class Slik():
+    def __init__(self, slik, pris, plads):
+        self.slik = slik
+        self.pris = pris
+        self.plads = plads
+
+
+    
