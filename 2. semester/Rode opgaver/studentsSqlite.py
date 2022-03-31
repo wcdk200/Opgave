23636bf54caa03948dd238b861cb89f50822ea6f
wcdk200 <sebguld@gmail.com>
2022-03-31 12:15:42 +0200

    fucktard

---
+++ 2. semester/Rode opgaver/studentsSqlite.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+class StudentDatabase():
+    def __init__(self, filename, create=False):
+        self.__conn = sqlite3.connect(filename)
+        self.__cur = self.__conn.cursor()
+        if create:
+            self.__cur.execute("DROP TABLE IF EXISTS Students")
+            self.__cur.execute("CREATE TABLE Students (sid INTEGER PRIMARY KEY, fn TEXT, ln TEXT, edu TEXT)")
+    def add(self, fn, ln, edu):
+        self.__cur.execute("INSERT INTO Students (fn, ln, edu) VALUES (?, ?, ?)", (fn, ln, edu))
+        self.__conn.commit()
+    def printALL(self):
+        self.__cur.execute("SELECT * FROM Students")
+        for row in self.__cur:
+            print(row)
+    def close(self):
+        self.__conn.close()
+    
+sdb = StudentDatabase("students.sqlite", True)
+sdb.add("will", "Smith", "None")
+sdb.add("Ole,", "Lukeoje", "None")
+sdb.printALL()
+sdb.close
