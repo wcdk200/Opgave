3089b029c580d6a859e1c27515f99ff24652481a
wcdk200 <sebguld@gmail.com>
2022-05-09 10:23:55 +0200

    m

---
+++ 2. semester/opgave 14/opgave14,2.py
@@ -0,0 +1,14 @@
+import threading
+import time
+
+def func(name):
+    for n in range(500):
+        print(name, end="")
+        time.sleep(0.5)
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target = func, args = ("T"))
+    t2 = threading.Thread(target = func, args = ("M"))
+
+t1.start()
+t2.start()
