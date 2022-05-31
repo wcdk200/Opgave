15ff11b4398a69978d07eb912c8e2c38c1a5c689
wcdk200 <sebguld@gmail.com>
2022-05-31 11:53:39 +0200

    m

---
+++ 2. semester/Opgave 17/opgave17,7.py
@@ -0,0 +1,29 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList():
+    def __init__(self):
+        self.__start = Node(None)
+    def printAll(self):
+        pass
+    def find(self, index):
+        pass
+    def remove(self, index):
+        pass
+
+
+class LinkedList():
+    class __Node():
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+        def __init__(self):
+            self.__start = self.__Node(None)
+        def printAll(self):
+            pass
+        def __find(self, index):
+            pass
+        def remove(self, index):
+            pass
