bca871b8756d005915536ec7e43ea629f426be07
wcdk200 <sebguld@gmail.com>
2022-08-24 19:06:26 +0200

    m

--- SoloOpgave/test.py
+++
@@ -1,9 +0,0 @@
-#se hvilken type en ting er (str,float osv.)
-print(type("u"))
-#Hvilken ord vi ikke må bruge
-import keyword
-print(keyword)
-#clear ting... men virker ikke helt
-import os
-clear = lambda: os.system("cls")
-print("hej")
